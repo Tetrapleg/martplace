ecc79b308e18eedeb205b810d80b6b8542098d1f
Tetrapleg <rufmama@yandex.ru>
2020-04-17 11:50:53 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function(){
 
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "14px",
+        readOnly: true
+    }); 
+
+
+    $('.slider__inner').slick({
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/lnr-chevron-left.svg" alt=""></button>',
+        // nextArrow: '<button class="slick-arrow slick-next"><img src="/images/lnr-chevron-right.svg" alt=""></button>',
+    });
 
     
 });
